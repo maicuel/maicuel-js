ae98a89a05b21d4b7185f05367d46455cf1b9356
maicuel <michael@maicuel.com>
2021-03-26 23:31:24 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('estamos conectados :)')
+
+
+let edad = prompt('Ingresa tu edad')
+let faltante =  18 - edad
+
+if ( edad >= 18 ) {
+    
+    let nombre = prompt('Ingresa tu nombre')
+    alert('Bienvenido ' + nombre )
+    console.log('cumple requisitos')
+
+} else {
+
+    alert('Lo siento no tienes edad suficiente para ver este contenido te faltan: ' + faltante + ' años' )
+    console.error('No cumple requisitos')
+}
+
