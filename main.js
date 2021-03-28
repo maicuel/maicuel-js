f562478857dcca274b61477d7afd61dc77537d62
maicuel <michael@maicuel.com>
2021-03-27 23:28:27 -0300

    ajustes en js

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
 console.log('estamos conectados :)')
 
-
-
 let nombre = prompt('Ingresa tu nombre')
-console.log(nombre)
 
+if ( nombre.length === 0 ){
 
-if (nombre.length > 0 && isNaN(nombre) ){
+    alert('Completa este campo!')
+    console.error('campo vacio')
 
-    let edad = parseInt(prompt('Ingresa tu edad'))
-    console.log(edad)
-
-    let faltante =  18 - edad
-
-    if (!isNaN(edad)) {
-        console.log('es un numero')
-
-        if ( edad >= 18 ) {
-        
-            alert('Bienvenido ' + nombre )
-            console.log('cumple requisitos')
-
-        } else {
-
-            alert('Lo siento ' + nombre + ' no tienes edad suficiente para ver este contenido te faltan: ' + faltante + ' años' )
-            console.error('No cumple requisitos')
-        }
-
-
-    } else {
-        console.log('no es un numero')
-        alert('Debes ingresar un numero')
-    }
-
-    
+} else if( !isNaN(nombre) ) {
+    alert('Completa este campo correctamente!')
+    console.error('El nombre no debe ser solo numeros')
 } else {
-    alert('Debes completar este campo correctamente!')}
+    let edad = prompt('ingresa tu edad')
 
+    edad = parseInt(edad)
+    
+    if ( edad <= 10 ) {
+        alert('Hola ' + nombre + ' debes tener mas de 10 años para ingresar')
+    } else if ( edad <= 17 ) {
+        alert( nombre + ' se que tienes ' + edad + ' años, pero te falta edad para ver todo el contenido. Por ahora solo puedes ver el contenido infantil')
+    } else if ( edad >= 18 && edad <= 109 ) {
+        alert('Muy bien ' + nombre + ' con ' + edad + ' años tienes acceso a todo nuestro contenido')
+    } else if( edad > 110) {
+        alert('De verdad tienes ' + edad + ' años! Revisa bien la edad que ingresaste!')
+    }
+}
